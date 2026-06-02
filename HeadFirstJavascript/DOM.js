cd4c14ee417cff136ee5bd19dbf776487245647a
Jeroen Piussi <r0615208@student.thomasmore.be>
2026-06-02 17:02:10 +0200

    followup course headsup

--- HeadFirstJavascript/DOM.js
+++ HeadFirstJavascript/DOM.js
@@ -1,6 +1,14 @@
 function init() {
     let greenplanet = document.getElementById("greenplanet");
     greenplanet.innerHTML = `Red Alert: hit by phaser fire!`;
+    greenplanet.setAttribute("class", "redtext");
+
+    if (!greenplanet == null){
+        console.log("Oh, I guess there isn't an alt attribute.");
+    } else {
+        console.log("I can't see the image in the console,");
+        console.log(" but I'm told it looks like " + greenplanet);
+    }
 }
 
 window.onload = init;
